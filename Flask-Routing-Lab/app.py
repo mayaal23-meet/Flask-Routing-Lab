e6a106093e4e2c6d15dc840bb28b5c2bd3eced4a
mayaal23-meet <109903937+mayaal23-meet@users.noreply.github.com>
2022-07-28 18:28:21 +0300

    r

--- Flask-Routing-Lab/app.py
+++ Flask-Routing-Lab/app.py
@@ -18,6 +18,10 @@ def home():
 def products():
 	return render_template('product.html')
 
+@app.route('/cart')
+def cart():
+	return render_template('cart.html')
+
 # Your code should be above
 
 if __name__ == "__main__":  # Makes sure this is the main process
